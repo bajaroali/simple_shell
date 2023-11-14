90db1add02f88d2569585b81601219002342622a
jimmyMithamo <j1mithamo@gmail.com>
2023-11-14 10:20:24 +0300

    return value

--- exit_shell.c
+++ exit_shell.c
@@ -3,6 +3,7 @@
 /**
  * exit_shell - exits the shell
  * exits with 0 for success
+ * Return: void
  */
 void exit_builtin(void)
 {
@@ -12,6 +13,7 @@ void exit_builtin(void)
 /**
  * signal_handler - a function to handle signals
  * @sig: the number of the signal
+ * Return: void
  */
 void signal_handler(int sig)
 {
